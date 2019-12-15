4a89e4f3258578be6b2cf4677b38884af867e8a4
jheepark <jhee.park.work@gmail.com>
2019-12-15 22:11:11 +1100

    acceleration based on mouse pos

--- nature-of-code/1/script.js
+++ nature-of-code/1/script.js
@@ -8,21 +8,48 @@ function setup() {
 
 function draw() {
   background(51);
-  w.walk();
+  w.update();
   w.display();
 }
 
 //constructor function
 //p5.js random function takes in a random( min, max ); --> gives you a random num between these 2
-function Walker() {
-  this.pos = createVector(width/2, height/2);
-  this.walk = function() {
-    this.pos.x = this.pos.x + random(-5, 5);
-    this.pos.y = this.pos.y + random(-5, 5);
-  }
+// function Walker() {
+// 	this.pos = createVector(width/2, height/2);
+// 	this.vel = createVector(0, 0);
 
-  this.display = function() {
-    fill(255);
-    ellipse(this.pos.x, this.pos.y, 48, 48);
-  }
+// 	this.update = function() {
+// 		this.acc = createVector(random(-1, 1), random(-1, 1));
+// 		this.acc.mult(5);
+
+// 		this.vel.add(this.acc);
+// 		this.pos.add(this.vel);
+// 	}
+
+//   this.display = function() {
+//     fill(255);
+//     ellipse(this.pos.x, this.pos.y, 48, 48);
+//   }
+// }
+
+// FOLLOWING MOUSE EXAMPLE
+
+function Walker() {
+	this.pos = createVector(width / 2, height / 2);
+	this.vel = createVector(0, 0);
+
+	this.update = function () {
+		var mouse = createVector(mouseX, mouseY);
+		this.acc = p5.Vector.sub(mouse, this.pos);
+		// this.acc.mult(0.01);
+		// this.acc.normalize();
+		this.acc.setMag(0.1);
+		this.vel.add(this.acc);
+		this.pos.add(this.vel);
+	}
+
+	this.display = function () {
+		fill(255);
+		ellipse(this.pos.x, this.pos.y, 48, 48);
+	}
 }
